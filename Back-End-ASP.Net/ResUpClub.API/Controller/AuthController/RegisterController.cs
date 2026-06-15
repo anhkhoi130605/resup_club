49cb5472a1dfb6e8d77eba364d9c06ecc2cbe63e
Khoi Pham <kpham6488@gmail.com>
2026-06-15 20:50:15 +0700

    17 feat hotfix

--- Back-End-ASP.Net/ResUpClub.API/Controller/AuthController/RegisterController.cs
+++ Back-End-ASP.Net/ResUpClub.API/Controller/AuthController/RegisterController.cs
@@ -53,7 +53,7 @@ public class RegisterController : ControllerBase
 
 		// 4. ĐIỀU HƯỚNG QUAY LẠI REACT: Đính Token lên URL để React bắt lấy
 		// (Bạn có thể đính thêm tên user, avatar nếu muốn, hoặc chỉ cần AccessToken là đủ)
-		var reactFrontendUrl = $"http://localhost:5173/login-success?token={loginResult.AccessToken}";
+		var reactFrontendUrl = $"httsp://localhost:5173/login-success?token={loginResult.AccessToken}";
 
 		return Redirect(reactFrontendUrl);
 	}
