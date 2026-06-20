34eaa7ad5afc0592a46d2670692456c2f15e80ba
Khoi Pham <kpham6488@gmail.com>
2026-06-20 22:35:30 +0700

    hotfix

--- Back-End-ASP.Net/ResUpClub.Application/Features/SystemModule/Notification/SendNotificationRequest.cs
+++ Back-End-ASP.Net/ResUpClub.Application/Features/SystemModule/Notification/SendNotificationRequest.cs
@@ -1,6 +1,6 @@
 using ResUpClub.Domain.Enums;
 
-namespace ResUpClub.Application.DTOs.Notification
+namespace ResUpClub.Application.Features.SystemModule.Notifications
 {
     public class SendNotificationRequest
     {
