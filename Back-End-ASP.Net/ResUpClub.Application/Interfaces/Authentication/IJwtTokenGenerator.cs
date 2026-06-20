0c4b8125be0e03cc454e9b84b9b52d9631278009
Khoi Pham <kpham6488@gmail.com>
2026-06-20 20:26:18 +0700

    20 config gitlab-ci.yml

--- Back-End-ASP.Net/ResUpClub.Application/Interfaces/Authentication/IJwtTokenGenerator.cs
+++ Back-End-ASP.Net/ResUpClub.Application/Interfaces/Authentication/IJwtTokenGenerator.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 using ResUpClub.Domain.Entities.AboutUser;
+using static ResUpClub.Domain.Enums.UserEnum;
 
 namespace ResUpClub.Application.Interfaces.Authentication
 {
 	public interface IJWTTokenGenerator
 	{
-		string GenerateToken(string userId, string email, IEnumerable<string> Role);
+		string GenerateToken(string userId, string email, IEnumerable<RoleEnum> Role);
 	}
 }
